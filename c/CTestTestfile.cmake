dc4cec04083f8f209ba28bbe93420471158aa9bf
David Hehir <david_hehir@hotmail.com>
2011-09-12 20:59:18 -0700

    moved c to ColorCorrelogram

--- c/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/david/Documents/src/comp3750/project/master/c
-# Build directory: /home/david/Documents/src/comp3750/project/master/c
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(src)
-SUBDIRS(test)
--- c/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/david/Documents/src/comp3750/project/master/c/test
-# Build directory: /home/david/Documents/src/comp3750/project/master/c/test
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(ColorCorrelogramTest "ColorCorrelogramTest")
-ADD_TEST(OpenCVHelperTest "OpenCVHelperTest")
