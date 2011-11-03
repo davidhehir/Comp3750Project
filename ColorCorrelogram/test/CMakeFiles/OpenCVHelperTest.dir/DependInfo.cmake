49283d25fb204e15bc952a18882fa9b4e37baf07
David Hehir <david_hehir@hotmail.com>
2011-11-03 13:11:07 +1100

    cleaned up files in ColorCorrelogram and FeatureMatcher

---
+++ ColorCorrelogram/test/CMakeFiles/OpenCVHelperTest.dir/DependInfo.cmake
@@ -0,0 +1,14 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_C
+  "/home/david/Documents/src/project/Comp3750Project/ColorCorrelogram/test/OpenCVHelperTest.c" "/home/david/Documents/src/project/Comp3750Project/ColorCorrelogram/test/CMakeFiles/OpenCVHelperTest.dir/OpenCVHelperTest.c.o"
+  )
+SET(CMAKE_C_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/david/Documents/src/project/Comp3750Project/ColorCorrelogram/src/CMakeFiles/OpenCVHelper.dir/DependInfo.cmake"
+  )
---
+++ ColorCorrelogram/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/david/Documents/src/project/Comp3750Project/ColorCorrelogram/test
+# Build directory: /home/david/Documents/src/project/Comp3750Project/ColorCorrelogram/test
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(ColorCorrelogramTest "ColorCorrelogramTest")
+ADD_TEST(OpenCVHelperTest "OpenCVHelperTest")
